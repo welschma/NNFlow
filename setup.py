b133ad51a16c2893a10728084ffae5e3cea59622
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2017-01-31 18:38:29 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='NNFlow',
+      version='0.1',
+      description='NNFlow framework to convert ROOT files to Tensorflow models',
+      url='https://github.com/kit-cn-cms/NNFlow',
+      author='KIT CN CMS team: Maximilian Welsch, Marco A. Harrendorf',
+      author_email='flyingcircus@example.com',
+      packages=['NNFlow'],
+      zip_safe=True)
